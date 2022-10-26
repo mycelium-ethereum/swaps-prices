c13d190c372cdbd534a9613fbbdaf951b549b0e4
Dospore <tetthera@gmail.com>
2022-10-26 18:40:32 +1100

    Update action

--- src/services/getPrices.ts
+++ src/services/getPrices.ts
@@ -2,6 +2,11 @@ type GetPriceArgs = {
   network?: any
 }
 
+const cachedPrices = {};
+
+// 2 second cache
+const expiry = 2 * 1000;
+
 export const getPrices = async (priceArgs: GetPriceArgs) => {
   return ({
     status: 400,
