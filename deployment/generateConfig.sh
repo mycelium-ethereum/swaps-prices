13cf3eb9d89f6569f0987a8efd0a428d3baf368e
Dospore <tetthera@gmail.com>
2022-10-27 14:43:35 +1100

    Add cert and configurable schema

---
+++ deployment/generateConfig.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p ./output
+rm -r ./output/*
+
+for filename in *.yml; do
+  ytt -f "$filename" -f "$1" >> "./output/$(basename "$filename" .yml).yml"
+done
+
+sed -i '' "s|IMAGE_NAME|$2|g" ./output/deployment.yml
