e096d29ee7c3e5852ce91445332a932df10ee0cc
Dospore <tetthera@gmail.com>
2022-10-27 15:55:01 +1100

    Update

--- deployment/generateConfig.sh
+++ deployment/generateConfig.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 mkdir -p ./output
-rm -rf ./output
+
+sed -i '' "s|IMAGE_NAME|$2|g" ./deployment.yml
 
 for filename in *.yml; do
   ytt -f "$filename" -f "$1" >> "./output/$(basename "$filename" .yml).yml"
 done
 
-sed -i '' "s|IMAGE_NAME|$2|g" ./output/deployment.yml
