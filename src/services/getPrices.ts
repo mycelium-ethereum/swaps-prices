b0ad414df4de3ddc3249301d31c62b7f2c15c516
Dospore <tetthera@gmail.com>
2022-11-02 16:47:34 +1100

    Return cached prices

--- src/services/getPrices.ts
+++ src/services/getPrices.ts
@@ -24,7 +24,10 @@ export const getPrices = async ({ network }: GetPriceArgs) => {
   const cache = cachedPrices[network];
 
   if (cache && cache.expiry < now) {
-    return cache.prices
+    return ({
+      status: HTTP_STATUS_CODE.OK,
+      body: cache.prices
+    })
   }
 
   const knownTokens = networkTokens[network];
