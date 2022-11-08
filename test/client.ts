deba67586260f2fa2bf507b6497382ff980f816a
Dospore <tetthera@gmail.com>
2022-11-08 12:17:47 +1100

    Test wss client

--- test/client.ts
+++ test/client.ts
@@ -1,6 +1,7 @@
+import {ethers} from 'ethers';
 import ws from 'ws';
-const client = new ws('ws://localhost:3030');
-// const client = new ws('ws://pricing.mycelium.xyz');
+// const client = new ws('ws://localhost:3030');
+const client = new ws('wss://pricing.mycelium.xyz');
 
 client.on('open', () => {
   console.log("Connectioned opened")
@@ -11,7 +12,13 @@ client.on('close', () => {
 });
 
 client.onmessage = (message) => {
-  console.log("Received update", parseRawWsMessage(message));
+  const msg = parseRawWsMessage(message);
+  const lastPrice = ethers.BigNumber.from(msg.l);
+  const newPrice = ethers.BigNumber.from(msg.p);
+  const delta = lastPrice.sub(newPrice).abs();
+  const diff = (delta.mul(ethers.utils.parseEther('1'))).div(lastPrice).mul(100)
+
+  console.log(`Received update`, { ...msg, difference: `${ethers.utils.formatEther(diff)}%`});
 }
 
 client.on('ping', () => {
